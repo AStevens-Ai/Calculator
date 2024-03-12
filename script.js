294c610ac256487798fc8f32564292dfed42753c
08ajs16@gmail.com <08ajs@Cloudzpc>
2024-03-12 07:55:53 -0400

    added styling and html structure of calculator

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ let inputOperator = '';
 
 let inputNumb2 = 0;
 
+const disp = document.querySelector('.display');
+
+const numBtn = document.querySelectorAll('.btn-nmb');
+
+const operBtn = document.querySelectorAll('.btn-operator');
+
+const clear = document.querySelector('.clear');
+
 function add(arr) {
   return arr.reduce((sum, curr) => sum + curr, 0);
 }
@@ -21,3 +29,5 @@ function multiply(a, b) {
 function divide(a, b) {
   return parseInt(a) / parseInt(b);
 }
+
+function operate(op, var1, var2) {}
